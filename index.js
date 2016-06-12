c229299b162644971d21ed8a3ecd5e7fef8775b6
Hendrixer <willscottmoss@gmail.com>
2016-06-11 20:38:11 -0700

    add(bin): add bin

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = {
-  gram: require('./gram')
+#!/usr/bin/env node
+
+var args = require('yargs').argv;
+
+if (!args.api) {
+  console.log('Must provide an api');
+  process.exit(1);
+} else {
+  require('./' + args.api);
 }
