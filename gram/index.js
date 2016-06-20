c6239700d8629f281cce9422e19ed5c9ef05602b
Hendrixer <willscottmoss@gmail.com>
2016-06-20 14:02:33 -0700

    remove(sessions)

--- gram/index.js
+++ gram/index.js
@@ -1,7 +1,8 @@
+
 const nodemon = require('nodemon');
 const path = require('path');
+const script = path.resolve(__dirname, 'server.js');
+const argv = require('yargs').argv;
+const port = argv.port || 3500;
 
-nodemon({
-  script: path.resolve(__dirname, 'server.js'),
-  ext: 'js'
-});
+nodemon(`-e "js" ${script} --port ${port}`);
--- gram/server.js
+++ gram/server.js
@@ -7,22 +7,8 @@ const DB = require('./db');
 const api = require('./api');
 const handleError = require('./handleError');
 const app = express();
-const argv = require('yargs').argv;
-
-const port = argv.port || 3500;
 
 app.set('trust proxy', 1) // trust first proxy
-app.use(cookieSession({
-  name: 'session',
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(function(req, res, next) {
-  const user = DB.get('users').find({id: 0}).value();
-  req.session.user = user;
-  next();
-})
 app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,4 +17,4 @@ app.use('/api', api());
 app.use(handleError());
 
 app.listen(port);
-console.log('API on port ' + port);
+console.log('Gram API on port ' + port);
