7a30c56ae24800bf98dd8ab04503508252759c96
Hendrixer <willscottmoss@gmail.com>
2016-06-21 00:56:11 -0700

    add(error)

--- gram/handleError.js
+++ gram/handleError.js
@@ -1,6 +1,10 @@
 module.exports = function() {
   return function(err, req, res, next) {
     console.error(err);
-    res.send(err.message);;
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send('Unauthorized, check JWT')
+    } else {
+      res.status(err.message);;
+    }
   }
 };
