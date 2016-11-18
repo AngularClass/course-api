f404b75663e6e7b8705dce3a1cb6de27ef958597
Scott Moss <willscottmoss@gmail.com>
2016-11-17 22:34:26 -0500

    add cors

--- cashed/src/middleware.js
+++ cashed/src/middleware.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { urlencoded, json } = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 module.exports = function(app) {
+  app.use(cors())
   app.use(morgan('dev'))
   app.use(urlencoded({extended: true}))
   app.use(json())
